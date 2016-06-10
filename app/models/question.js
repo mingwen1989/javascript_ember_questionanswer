1db4ed55f536a748891fe8bdd6f25e86d307c59e
themingfu <mwen82@gmail.com>
2016-06-10 16:42:29 -0700

    Add display answer function

--- app/models/question.js
+++ app/models/question.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
     author: DS.attr(),
     contents: DS.attr(),
     notes: DS.attr(),
-    title: DS.attr()
+    title: DS.attr(),
+    answers: DS.hasMany('answer', {async: true})
 });
