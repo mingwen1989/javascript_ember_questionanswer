71425d8b14ec0043605fbc6a259612777bb9a4a7
themingfu <mwen82@gmail.com>
2016-06-17 14:50:28 -0700

    Style reading list page

--- app/components/cart-counter.js
+++ app/components/cart-counter.js
@@ -2,10 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   readingList: Ember.inject.service(),
-  actions: {
-    itemCount() {
-      // console.log(this.get('shoppingCart'));
-      // return this.get('shoppingCart').get('cars').length;
-    }
-  }
 });
--- app/helpers/cart-popup.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export function cartPopup(params/*, hash*/) {
-  return params[0];
-}
-
-export default Ember.Helper.helper(cartPopup);
