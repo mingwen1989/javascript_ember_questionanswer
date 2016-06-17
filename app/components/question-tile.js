a5c41cfcf4debb1a094823ab54e1a0ffc4ead7bb
themingfu <mwen82@gmail.com>
2016-06-17 09:15:43 -0700

    Add custom helper to display fire icon near popular question

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  commentCount: Ember.computed('question.answers.length', function(){
+    return this.get('question.answers.length');
+  }),
   actions: {
     update(rental, params) {
       this.sendAction('update', rental, params);
---
+++ tests/unit/helpers/answer-popularity-test.js
@@ -0,0 +1,10 @@
+import { answerPopularity } from 'question-answer/helpers/answer-popularity';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | answer popularity');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = answerPopularity([42]);
+  assert.ok(result);
+});
